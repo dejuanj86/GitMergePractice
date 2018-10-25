ec268e4065a8d80a91824191d2bf98aa667ef966
Dejuan Johnson <dejuanj86@gmail.com>
2018-10-25 13:44:18 -0400

    added a line

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,6 +4,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Goodbye World");
+		System.out.println("My name is Mad Man Juan");
 	}
 
 }
